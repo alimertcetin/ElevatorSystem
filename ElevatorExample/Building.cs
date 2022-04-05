878a2ed67b0c4bba1bcb96bdd1f273d47a6ea603
alimertcetin <mrtmert54@gmail.com>
2022-04-06 01:58:56 +0300

    Possible bug fix
    - Cant select correct elevator when calling multiple at ones

--- ElevatorExample/Building.cs
+++ ElevatorExample/Building.cs
@@ -47,7 +47,7 @@ namespace AutamationSystem.BuildingSytem
             Floors = new Floor[floorCount]; //initialize array
             for (int i = 0; i < floorCount; i++)
             {
-                Floors[i] = new Floor(this, i, elevatorCount); //initilize floors inside of the array
+                Floors[i] = new Floor(i, elevatorCount); //initilize floors inside of the array
                 for (int j = 0; j < Floors[i].elevatorButtons.Length; j++)
                 {
                     Floors[i].elevatorButtons[j].onButtonPressed += OnElevatorCalled;
@@ -57,28 +57,58 @@ namespace AutamationSystem.BuildingSytem
 
         private void OnElevatorCalled(Button button)
         {
+            //TODO : It seems working but needs cleanup
             var callButton = button as ElevatorCallButton;
 
-            Elevator selectedElevator = null;
+            List<Elevator> possibleAvailableList = new List<Elevator>();
             foreach (Elevator elevator in Elevators)
             {
-                Direction directionToThisFloor = elevator.GetDirection(callButton.Floor.FloorIndex);
-                if (elevator.TargetFloors.Count > 0)
+                //Direction directionToThisFloor = elevator.GetDirection(callButton.Floor.FloorIndex);
+                if (elevator.TargetFloors.Count == 0)
                 {
-                    Direction directionToNextFloor = elevator.GetDirection(elevator.TargetFloors[0].FloorIndex);
-                    if (directionToThisFloor == directionToNextFloor)
-                    {
-                        elevator.AddTarget(callButton.Floor, elevator.CurrentDirection);
-                        selectedElevator = elevator;
-                        break;
-                    }
+                    possibleAvailableList.Add(elevator);
                 }
             }
+
+            Elevator selectedElevator = null;
+            var closest = Elevator.GetClosest(Elevators, callButton.Floor.FloorIndex);
+
+            if(closest.TargetFloors.Count == 0)
+            {
+                selectedElevator = closest;
+                selectedElevator.AddTarget(callButton.Floor, callButton.ButtonDirection);
+                return;
+            }
+
+            if(!possibleAvailableList.Contains(closest)) possibleAvailableList.Add(closest);
+
+            foreach (Elevator elevator in possibleAvailableList)
+            {
+                if(elevator.TargetFloors.Count > 0 &&
+                    callButton.ButtonDirection == elevator.CurrentDirection &&
+                    elevator.GetDirection(callButton.Floor.FloorIndex) == elevator.CurrentDirection)
+                {
+                    selectedElevator = elevator;
+                    break;
+                }
+                if (elevator.TargetFloors.Count == 0)
+                {
+                    selectedElevator = elevator;
+                    break;
+                }
+            }
+
             if (selectedElevator == null)
             {
-                var closest = Elevator.GetClosest(Elevators, callButton.Floor.FloorIndex);
-                closest.AddTarget(callButton.Floor, callButton.ButtonDirection);
+                selectedElevator = closest;
+                /*
+                 * Available conditions :
+                 *      - Going to buttons direction ex : Up and elevator's direction to floor is also same (Up)
+                 *      - If there is no elevator that matches the conditions
+                 *          - Add to queue and next frame try to cleanup?
+                 */
             }
+            selectedElevator.AddTarget(callButton.Floor, callButton.ButtonDirection);
         }
 
         private void InıtializeElevators(int elevatorCount)
