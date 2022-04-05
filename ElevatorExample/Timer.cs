3272506bd1b7632d0f8e91fb9f7867522a1e152f
alimertcetin <mrtmert54@gmail.com>
2022-04-06 01:56:28 +0300

    Created simple timer class

---
+++ ElevatorExample/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace XIV
+{
+    public static class Timer
+    {
+        public static float DeltaTime;
+
+        static TimeSpan previousTime;
+
+        static Timer()
+        {
+            DeltaTime = 0;
+            previousTime = DateTime.Now.TimeOfDay;
+        }
+
+        public static void Update()
+        {
+            TimeSpan currentTime = DateTime.Now.TimeOfDay;
+            DeltaTime = (float)currentTime.Subtract(previousTime).TotalMilliseconds / 1000;
+            previousTime = currentTime;
+        }
+    }
+}
